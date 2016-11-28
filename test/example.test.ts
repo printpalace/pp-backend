065a7d25de9b2fd97a48008307a17a31751fd328
Artem Oliinyk <artem.oliinyk@eleks.com>
2016-11-28 18:12:44 +0200

    Fill ts, hint, nodemon, nyc configs

---
+++ test/example.test.ts
@@ -0,0 +1,23 @@
+import 'mocha';
+import {assert} from 'chai';
+
+function add() {
+  return Array.prototype.slice.call(arguments).reduce(function (prev, curr) {
+    return prev + curr;
+  }, 0);
+}
+
+describe('add()', function () {
+  const tests = [
+    {args: [1, 2], expected: 3},
+    {args: [1, 2, 3], expected: 6},
+    {args: [1, 2, 3, 4], expected: 10}
+  ];
+
+  tests.forEach(function (test) {
+    it('correctly adds ' + test.args.length + ' args', function () {
+      const res = add.apply(null, test.args);
+      assert.equal(res, test.expected);
+    });
+  });
+});
